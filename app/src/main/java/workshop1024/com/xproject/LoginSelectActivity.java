51d2db926d8d8b5fad719d4380cf14b12742f1dd
chengxiang.peng <chengxiang.peng@qunar.com>
2017-11-26 16:39:18 +0800

    添加登陆选择页面

--- app/src/main/java/workshop1024/com/xproject/LoginSelectActivity.java
+++ app/src/main/java/workshop1024/com/xproject/LoginSelectActivity.java
@@ -3,11 +3,14 @@ package workshop1024.com.xproject;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+/**
+ * 登陆选择页面
+ */
+public class LoginSelectActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loginselect);
     }
 }
