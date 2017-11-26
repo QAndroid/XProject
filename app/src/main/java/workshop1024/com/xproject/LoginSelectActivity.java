c865c8d267a7cb162719c70c8bb2de4882c2d9dd
chengxiang.peng <chengxiang.peng@qunar.com>
2017-11-26 18:00:09 +0800

    引入Data Binding Library，简单处理按钮点击事件

--- app/src/main/java/workshop1024/com/xproject/LoginSelectActivity.java
+++ app/src/main/java/workshop1024/com/xproject/LoginSelectActivity.java
@@ -1,7 +1,12 @@
 package workshop1024.com.xproject;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import workshop1024.com.xproject.databinding.LoginselectActivityBinding;
 
 /**
  * 登陆选择页面
@@ -11,6 +16,18 @@ public class LoginSelectActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginselect);
+        LoginselectActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.loginselect_activity);
+        LoginSelectHandlers handlers = new LoginSelectHandlers();
+        binding.setHandlers(handlers);
+    }
+
+    public class LoginSelectHandlers {
+        public void onClickLogin(View view) {
+            Toast.makeText(LoginSelectActivity.this, "login click", Toast.LENGTH_SHORT).show();
+        }
+
+        public void onClickTry(View view) {
+            Toast.makeText(LoginSelectActivity.this, "try click", Toast.LENGTH_SHORT).show();
+        }
     }
 }
