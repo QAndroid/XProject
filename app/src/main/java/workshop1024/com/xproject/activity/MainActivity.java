82596f7afbd9ffcb2a76acb510714c3731b3e049
chengxiang.peng <chengxiang.peng@qunar.com>
2017-12-09 15:32:45 +0800

    主页Toolbar更新逻辑

--- app/src/main/java/workshop1024/com/xproject/activity/MainActivity.java
+++ app/src/main/java/workshop1024/com/xproject/activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -25,9 +26,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private MainActivityBinding mBinding;
     private FragmentManager mFragmentManager;
 
+    private int mSelectItemId = R.id.leftnavigator_menu_home;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("TAG", "onCreate");
         mBinding = DataBindingUtil.setContentView(this, R.layout.main_activity);
 
         setSupportActionBar(mBinding.layoutRight.toolbarNavigator);
@@ -42,18 +46,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         mFragmentManager = getSupportFragmentManager();
         //默认展示HomeFragment
-        replaceHomeFragment();
+        showHomeFragment();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i("TAG", "onCreateOptionsMenu");
         //创建ActionBar右边Handle actions
-        getMenuInflater().inflate(R.menu.main_toolbar_actions, menu);
-        return true;
+        if (mSelectItemId == R.id.leftnavigator_menu_home) {
+            getMenuInflater().inflate(R.menu.home_toolbar_actions, menu);
+        } else if (mSelectItemId == R.id.leftnavigator_menu_saved) {
+            getMenuInflater().inflate(R.menu.saved_toolbar_actions, menu);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Log.i("TAG", "onPrepareOptionsMenu");
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i("TAG", "onOptionsItemSelected");
         int id = item.getItemId();
         if (id == R.id.toolbar_action_search) {
             Toast.makeText(this, "toolbar_action_search", Toast.LENGTH_SHORT).show();
@@ -76,28 +92,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
+        Log.i("TAG", "onNavigationItemSelected");
+        mSelectItemId = item.getItemId();
 
-        if (id == R.id.leftnavigator_menu_home) {
-            replaceHomeFragment();
-        } else if (id == R.id.leftnavigator_menu_saved) {
+        if (mSelectItemId == R.id.leftnavigator_menu_home) {
+            showHomeFragment();
+        } else if (mSelectItemId == R.id.leftnavigator_menu_saved) {
             SavedFragment savedFragment = SavedFragment.newInstance();
             mFragmentManager.beginTransaction().replace(R.id.framelayout_fragments, savedFragment).commit();
-        } else if (id == R.id.leftnavigator_menu_settings) {
+            setTitle(getResources().getString(R.string.leftnavigator_menu_saved));
+        } else if (mSelectItemId == R.id.leftnavigator_menu_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
-        } else if (id == R.id.leftnavigator_menu_feedback) {
+        } else if (mSelectItemId == R.id.leftnavigator_menu_feedback) {
             Intent intent = new Intent(this, FeedbackActivity.class);
             startActivity(intent);
         }
 
         mBinding.drawerlayoutNavigator.closeDrawer(GravityCompat.START);
+        invalidateOptionsMenu();
+
         return true;
     }
 
 
-    private void replaceHomeFragment() {
+    private void showHomeFragment() {
         HomeFragment homeFragment = HomeFragment.newInstance();
         mFragmentManager.beginTransaction().replace(R.id.framelayout_fragments, homeFragment).commit();
+        setTitle(getResources().getString(R.string.leftnavigator_menu_home));
     }
 }
