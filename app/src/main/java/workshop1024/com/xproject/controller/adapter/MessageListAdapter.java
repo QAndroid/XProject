fed7b408296ac75e4b21bff5e56cf0cd1838cbdb
chengxiang.peng <chengxiang.peng@qunar.com>
2018-06-10 17:58:55 +0800

    完成了反馈页面耳机列表的开发

--- app/src/main/java/workshop1024/com/xproject/controller/adapter/MessageListAdapter.java
+++ app/src/main/java/workshop1024/com/xproject/controller/adapter/MessageListAdapter.java
@@ -1,6 +1,7 @@
 package workshop1024.com.xproject.controller.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,23 +11,25 @@ import java.util.List;
 
 import workshop1024.com.xproject.R;
 import workshop1024.com.xproject.model.message.Message;
+import workshop1024.com.xproject.model.message.MessageGroup;
 
 /**
  * 消息列表是配资
  * TDDO 如何抽象出一个整体的HeaderView和FooterView、EmptyView的适配器
  */
-public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.MessageViewHolder> {
+public class MessageListAdapter extends RecyclerView.Adapter {
     public static final int TYPEP_HEADER = 0;
     public static final int TYPE_FOOTER = 1;
-    public static final int TYPE_NORMAL = 2;
+    public static final int TYPE_GROUP = 2;
+    public static final int TYPE_CHILD = 3;
 
-    private List<Message> mMessageList;
+    private List<MessageGroup> mMessageGroupList;
 
     private View mHeaderView;
     private View mFooterView;
 
-    public MessageListAdapter(List<Message> messageList) {
-        mMessageList = messageList;
+    public MessageListAdapter(List<MessageGroup> messageGroupList) {
+        mMessageGroupList = messageGroupList;
     }
 
     public void setHeaderView(View headerView) {
@@ -34,75 +37,184 @@ public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.
         notifyItemInserted(0);
     }
 
-
     public void setFooterView(View footerView) {
         mFooterView = footerView;
-        notifyItemInserted(getItemCount() - 1);
+        notifyItemInserted(getMessageGroupItemCount() - 1);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.i("XProject", "onCreateViewHolder()");
+        RecyclerView.ViewHolder viewHolder = null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+        if (mHeaderView != null && viewType == TYPEP_HEADER) {
+            Log.i("XProject", "mHeaderView != null && viewType == TYPEP_HEADER");
+            viewHolder = new HeaderViewHolder(mHeaderView);
+        } else if (mFooterView != null && viewType == TYPE_FOOTER) {
+            viewHolder = new FooterViewHolder(mFooterView);
+            Log.i("XProject", "mFooterView != null && viewType == TYPE_FOOTER");
+        } else if (viewType == TYPE_GROUP) {
+            Log.i("XProject", "viewType == TYPE_GROUP");
+            View groupView = layoutInflater.inflate(R.layout.messagelist_item_group, parent, false);
+            viewHolder = new GroupViewHolder(groupView);
+        } else if (viewType == TYPE_CHILD) {
+            Log.i("XProject", "viewType == TYPE_CHILD");
+            View childView = layoutInflater.inflate(R.layout.messagelist_item_child, parent, false);
+            viewHolder = new ChildViewHolder(childView);
+        }
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Log.i("XProject", "onBindViewHolder()");
+        if (holder instanceof GroupViewHolder) {
+            Log.i("XProject", "holder instanceof GroupViewHolder.position = " + position + ",text = " + getItemTextByPosition(position));
+            ((GroupViewHolder) holder).mDataTextView.setText(getItemTextByPosition(position));
+        } else if (holder instanceof ChildViewHolder) {
+            Log.i("XProject", "holder instanceof ChildViewHolder = " + position + ",text = " + getItemTextByPosition(position));
+            ((ChildViewHolder) holder).mContentTextView.setText(getItemTextByPosition(position));
+        }
+    }
+
+    private String getItemTextByPosition(int position) {
+        if (mHeaderView != null) {
+            position = position - 1;
+        }
+
+        String itemText = null;
+
+        for (MessageGroup messageGroup : mMessageGroupList) {
+            if (position == 0) {
+                itemText = messageGroup.getPublishData();
+            }
+            position--;
+            for (Message message : messageGroup.getMessageList()) {
+                if (position == 0) {
+                    itemText = message.getContent();
+                }
+                position--;
+            }
+        }
+
+        return itemText;
     }
 
     @Override
     public int getItemViewType(int position) {
+        Log.i("XProject", "getItemViewType()");
+        int viewType;
+
         if (mHeaderView == null && mFooterView == null) {
-            return TYPE_NORMAL;
-        }
-        if (position == 0) {
-            return TYPEP_HEADER;
-        }
-        if (position == getItemCount() - 1) {
-            return TYPE_FOOTER;
-        }
-        return TYPE_NORMAL;
-    }
-
-    @Override
-    public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (mHeaderView != null && viewType == TYPEP_HEADER) {
-            return new MessageViewHolder(mHeaderView);
-        }
-
-        if (mFooterView != null && viewType == TYPE_FOOTER) {
-            return new MessageViewHolder(mFooterView);
-        }
-
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.messagelist_item_content,
-                parent, false);
-        return new MessageViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(MessageViewHolder holder, int position) {
-        if (getItemViewType(position) == TYPE_NORMAL) {
-            holder.mMessageTextView.setText(mMessageList.get(position - 1).getContent());
-            return;
-        } else if (getItemViewType(position) == TYPEP_HEADER) {
-            return;
+            Log.i("XProject", "mHeaderView == null && mFooterView == null");
+            viewType = getMessageTypeByPosition(position);
+        } else if (mHeaderView != null && mFooterView == null) {
+            if (position == 0) {
+                viewType = TYPEP_HEADER;
+            } else {
+                viewType = getMessageTypeByPosition(position - 1);
+            }
+        } else if (mHeaderView == null && mFooterView != null) {
+            if (position == getItemCount() - 1) {
+                viewType = TYPE_FOOTER;
+            } else {
+                viewType = getMessageTypeByPosition(position);
+            }
         } else {
-            return;
+            if (position == 0) {
+                viewType = TYPEP_HEADER;
+            } else if (position == getItemCount() - 1) {
+                viewType = TYPE_FOOTER;
+            } else {
+                viewType = getMessageTypeByPosition(position - 1);
+            }
         }
+
+        Log.i("XProject", "position = " + position + ",viewType = " + viewType);
+        return viewType;
+    }
+
+    private int getMessageTypeByPosition(int position) {
+        int viewType = 0;
+
+        for (MessageGroup messageGroup : mMessageGroupList) {
+            if (position == 0) {
+                viewType = TYPE_GROUP;
+                return viewType;
+            }
+            position--;
+            for (Message message : messageGroup.getMessageList()) {
+                if (position == 0) {
+                    viewType = TYPE_CHILD;
+                    return viewType;
+                }
+                position--;
+            }
+        }
+
+        return viewType;
     }
 
     @Override
     public int getItemCount() {
-        if (mHeaderView == null && mFooterView == null) {
-            return mMessageList.size();
-        } else if ((mHeaderView == null && mFooterView != null) || (mHeaderView != null &&
-                mFooterView == null)) {
-            return mMessageList.size() + 1;
+        Log.i("XProject", "getItemCount()");
+        int itemCount;
+        if (mHeaderView != null && mFooterView != null) {
+            Log.i("XProject", "mHeaderView != null && mFooterView != null");
+            itemCount = getMessageGroupItemCount() + 2;
+        } else if (mHeaderView == null && mFooterView == null) {
+            Log.i("XProject", "mHeaderView == null && mFooterView == null");
+            itemCount = getMessageGroupItemCount();
         } else {
-            return mMessageList.size() + 2;
+            itemCount = getMessageGroupItemCount() + 1;
+        }
+
+        Log.i("XProject", "itemCount = " + itemCount);
+        return itemCount;
+    }
+
+    public int getMessageGroupItemCount() {
+        int itemCount = 0;
+        for (MessageGroup messageGroup : mMessageGroupList) {
+            itemCount++;
+            for (Message message : messageGroup.getMessageList()) {
+                itemCount++;
+            }
+        }
+        return itemCount;
+    }
+
+    public class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
         }
     }
 
-    public class MessageViewHolder extends RecyclerView.ViewHolder {
-        public TextView mMessageTextView;
+    public class GroupViewHolder extends RecyclerView.ViewHolder {
+        private TextView mDataTextView;
 
-        public MessageViewHolder(View itemView) {
+        public GroupViewHolder(View itemView) {
             super(itemView);
-            if (itemView == mHeaderView || itemView == mFooterView) {
-                return;
-            }
+            mDataTextView = itemView.findViewById(R.id.messagelist_textview_groupdata);
+        }
+    }
 
-            mMessageTextView = itemView.findViewById(R.id.messagelist_textview_content);
+    public class ChildViewHolder extends RecyclerView.ViewHolder {
+        public TextView mContentTextView;
+
+        public ChildViewHolder(View itemView) {
+            super(itemView);
+            mContentTextView = itemView.findViewById(R.id.messagelist_textview_childcontent);
+        }
+    }
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+
+        public FooterViewHolder(View itemView) {
+            super(itemView);
         }
     }
 }
