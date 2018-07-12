8b8fdc68320170da8746af350b021a00cd2f2de6
chengxiang.peng <chengxiang.peng@qunar.com>
2018-07-13 01:41:58 +0800

    初步完成CompactAdapter页面的DataBinding优化

--- app/src/main/java/workshop1024/com/xproject/controller/adapter/CompactAdapter.java
+++ app/src/main/java/workshop1024/com/xproject/controller/adapter/CompactAdapter.java
@@ -1,17 +1,17 @@
 package workshop1024.com.xproject.controller.adapter;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.List;
 
 import workshop1024.com.xproject.R;
 import workshop1024.com.xproject.controller.activity.home.NewsDetailActivity;
+import workshop1024.com.xproject.databinding.NewslistItemCompactBinding;
 import workshop1024.com.xproject.model.news.News;
 
 /**
@@ -20,7 +20,6 @@ import workshop1024.com.xproject.model.news.News;
 public class CompactAdapter extends RecyclerView.Adapter<CompactAdapter.NewsViewHolder> {
     private Context mContext;
     private List<News> mNewsList;
-    private News mNews;
 
     public CompactAdapter(Context context, List<News> newsList) {
         mContext = context;
@@ -29,17 +28,15 @@ public class CompactAdapter extends RecyclerView.Adapter<CompactAdapter.NewsView
 
     @Override
     public NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.newslist_item_compact, parent,
-                false);
-        return new NewsViewHolder(view);
+        NewslistItemCompactBinding newslistItemCompactBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                R.layout.newslist_item_compact, parent, false);
+        newslistItemCompactBinding.setCompactHandlers(new CompactHandlers());
+        return new NewsViewHolder(newslistItemCompactBinding);
     }
 
     @Override
     public void onBindViewHolder(NewsViewHolder holder, int position) {
-        holder.mNews = mNewsList.get(position);
-        holder.mTitleTextView.setText(mNewsList.get(position).getTitle());
-        holder.mAuthorTextView.setText(mNewsList.get(position).getPublisher());
-        holder.mTimeTextView.setText(mNewsList.get(position).getPubDate());
+        holder.mNewslistItemCompactBinding.setNews(mNewsList.get(position));
     }
 
     @Override
@@ -47,26 +44,18 @@ public class CompactAdapter extends RecyclerView.Adapter<CompactAdapter.NewsView
         return mNewsList.size();
     }
 
-    public class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private final ImageView mBannerImageView;
-        private final TextView mTitleTextView;
-        private final TextView mAuthorTextView;
-        private final TextView mTimeTextView;
-        private News mNews;
+    public class NewsViewHolder extends RecyclerView.ViewHolder {
+        private final NewslistItemCompactBinding mNewslistItemCompactBinding;
 
-        private NewsViewHolder(View view) {
-            super(view);
-            view.setOnClickListener(this);
-
-            mBannerImageView = view.findViewById(R.id.compact_imageview_banner);
-            mTitleTextView = view.findViewById(R.id.compact_textview_title);
-            mAuthorTextView = view.findViewById(R.id.compact_textview_author);
-            mTimeTextView = view.findViewById(R.id.compact_textview_time);
+        private NewsViewHolder(NewslistItemCompactBinding newslistItemCompactBinding) {
+            super(newslistItemCompactBinding.getRoot());
+            mNewslistItemCompactBinding = newslistItemCompactBinding;
         }
+    }
 
-        @Override
-        public void onClick(View view) {
-            NewsDetailActivity.startActivity(mContext, mNews.getNewId());
+    public class CompactHandlers {
+        public void onClickItem(View view, News news) {
+            NewsDetailActivity.startActivity(mContext, news.getNewId());
         }
     }
 }
