21ff6be856afadff44a01f987be0b4ccbfb8d61e
chengxiang.peng <chengxiang.peng@qunar.com>
2018-07-16 12:09:01 +0800

    初步提交GrassView databinding代码

--- app/src/main/java/workshop1024/com/xproject/view/group/GrassView.java
+++ app/src/main/java/workshop1024/com/xproject/view/group/GrassView.java
@@ -4,20 +4,20 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import workshop1024.com.xproject.R;
+import workshop1024.com.xproject.databinding.GrassViewBinding;
 
 /**
  * 草视图
  */
 public class GrassView extends LinearLayout {
-    private ImageView mGrassImageView;
-    private ImageView mShadowImageView;
+    private GrassViewBinding mGrassViewBinding;
 
     public GrassView(Context context) {
         super(context);
@@ -31,29 +31,26 @@ public class GrassView extends LinearLayout {
 
     private void initViewShow(Context context) {
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        layoutInflater.inflate(R.layout.grass_view, this);
-
-        mGrassImageView = findViewById(R.id.grassview_imageview_grass);
-        mShadowImageView = findViewById(R.id.grassview_imageview_shadow);
+        mGrassViewBinding = DataBindingUtil.inflate(layoutInflater, R.layout.grass_view, this, false);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
 
-        ObjectAnimator translationXAnimator = ObjectAnimator.ofFloat(mGrassImageView, "translationX",
-                -mGrassImageView.getWidth(), getWidth() + mGrassImageView.getWidth());
+        ObjectAnimator translationXAnimator = ObjectAnimator.ofFloat(mGrassViewBinding.grassviewImageviewGrass, "translationX",
+                -mGrassViewBinding.grassviewImageviewGrass.getWidth(), getWidth() + mGrassViewBinding.grassviewImageviewGrass.getWidth());
         translationXAnimator.setDuration(10000);
         translationXAnimator.setRepeatMode(ValueAnimator.RESTART);
         translationXAnimator.setRepeatCount(1000);
 
-        ObjectAnimator translationYAnimator = ObjectAnimator.ofFloat(mGrassImageView, "translationY", 0, -50);
+        ObjectAnimator translationYAnimator = ObjectAnimator.ofFloat(mGrassViewBinding.grassviewImageviewGrass, "translationY", 0, -50);
         translationYAnimator.setDuration(1000);
         translationYAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
         translationYAnimator.setRepeatCount(1000);
         translationYAnimator.setRepeatMode(ValueAnimator.REVERSE);
 
-        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(mGrassImageView, "rotation", 0, 3600);
+        ObjectAnimator rotationAnimator = ObjectAnimator.ofFloat(mGrassViewBinding.grassviewImageviewGrass, "rotation", 0, 3600);
         rotationAnimator.setDuration(10000);
         rotationAnimator.setRepeatCount(1000);
         rotationAnimator.setRepeatMode(ValueAnimator.RESTART);
@@ -62,8 +59,8 @@ public class GrassView extends LinearLayout {
         grassAnimatorSet.playTogether(translationXAnimator, translationYAnimator, rotationAnimator);
         grassAnimatorSet.start();
 
-        ObjectAnimator translationXAnimator2 = ObjectAnimator.ofFloat(mShadowImageView, "translationX",
-                -mShadowImageView.getWidth(), getWidth() + mShadowImageView.getWidth());
+        ObjectAnimator translationXAnimator2 = ObjectAnimator.ofFloat(mGrassViewBinding.grassviewImageviewShadow, "translationX",
+                -mGrassViewBinding.grassviewImageviewShadow.getWidth(), getWidth() + mGrassViewBinding.grassviewImageviewShadow.getWidth());
         translationXAnimator2.setDuration(10000);
         translationXAnimator2.setRepeatMode(ValueAnimator.RESTART);
         translationXAnimator2.setRepeatCount(1000);
