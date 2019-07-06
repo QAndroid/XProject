ff1a3c5ed48b733cde3ab87d065e2281c894e8fd
chengxiang.peng <chengxiang.peng@qunar.com>
2019-07-06 17:14:47 +0800

    迁移Appliction和Constant

--- app/src/main/java/workshop1024/com/xproject/XConstant.java
+++
@@ -1,5 +0,0 @@
-package workshop1024.com.xproject;
-
-public class XConstant {
-    public static final String SERVER_URL = "http://192.168.99.118:10800";
-}
---
+++ app/src/main/java/workshop1024/com/xproject/XConstant.kt
@@ -0,0 +1,7 @@
+package workshop1024.com.xproject
+
+class XConstant {
+    companion object {
+        const val SERVER_URL: String = "http://192.168.99.118:10800"
+    }
+}
