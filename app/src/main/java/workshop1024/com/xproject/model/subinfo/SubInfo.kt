dd8a13c2c042ea6aee930016fa0f1cef05d1cb9b
chengxiang.peng <chengxiang.peng@qunar.com>
2019-07-10 00:43:50 +0800

    迁移了"SubInfo"

---
+++ app/src/main/java/workshop1024/com/xproject/model/subinfo/SubInfo.kt
@@ -0,0 +1,7 @@
+package workshop1024.com.xproject.model.subinfo
+
+//Info的id
+//Info图标URL
+//Info名称
+//Info未阅读新闻数量
+data class SubInfo(var infoId: String, var iconUrl: String, var name: String, var unreadCount: String)
