aae1b4012b0b18dd6729effc5039bfad630b43d3
chengxiang.peng <chengxiang.peng@qunar.com>
2019-07-11 10:17:14 +0800

    迁移了"SubInfoRepository"

--- app/src/prod/java/workshop1024/com/xproject/model/Injection.java
+++ app/src/prod/java/workshop1024/com/xproject/model/Injection.java
@@ -35,6 +35,6 @@ public class Injection {
     }
 
     public static SubInfoDataSource provideSubInfoRepository() {
-        return SubInfoRepository.getInstance();
+        return SubInfoRepository.Companion.getInstance();
     }
 }
