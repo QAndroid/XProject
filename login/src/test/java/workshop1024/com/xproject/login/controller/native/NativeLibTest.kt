f56203add5f57859cc85937528225638862b815c
ChengXiang.Peng <106783860@qq.com>
2019-11-23 10:41:05 +0800

    提交FIXME TODO NDK单元测试自动化打包

--- login/src/test/java/workshop1024/com/xproject/login/controller/native/NativeLibTest.kt
+++ login/src/test/java/workshop1024/com/xproject/login/controller/native/NativeLibTest.kt
@@ -8,6 +8,9 @@ import java.io.File
 class NativeLibTest {
     @Test
     fun test_stringFromJNI() {
+        //FIXME 在自动化测试的时候自动打包dylib包，目前还只能提前手工打包
+        //参考：https://www.jianshu.com/p/e5270ba1c11d
+
         //java.lang.UnsatisfiedLinkError: Directory separator should not appear in library name
         //no suitable image found.  Did find: libnative-lib.dylib: file too short
         //System.load，参数必须是绝对路劲
